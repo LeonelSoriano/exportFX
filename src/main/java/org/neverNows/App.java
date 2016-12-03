85efd40dc2e725ec5c6739cb2f6527d40552410a
leonel soriano <leonelsoriano3@gmail.com>
2016-12-03 18:00:45 -0400

    primer commir

---
+++ src/main/java/org/neverNows/App.java
@@ -0,0 +1,45 @@
+package org.neverNows;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Application.launch(args);
+    }
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Button bt = new Button("boton");
+        bt.setOnAction(new EventHandler<ActionEvent>() 
+        {
+            @Override
+            public void handle(ActionEvent arg0)
+            {
+                System.out.println("HelloWorld! :)");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 300, 150);
+        root.getChildren().add(bt);
+
+        stage.setTitle("HelloWorld in JavaFX 2.0");
+        stage.setScene(scene);
+        stage.show();
+		
+	}
+}
---
+++ src/test/java/org/neverNows/AppTest.java
@@ -0,0 +1,28 @@
+package org.neverNows;
+
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( )
+    {
+       
+    }
+    
+    @Test
+    public void ejemplo(){
+    	assertTrue(true);
+    	
+    }
+
+
+}
